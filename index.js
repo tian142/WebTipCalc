d3aa84512ea95ceaeab35d2e97867476174ab193
Tian Qiu <tian.qiu142@gmail.com>
2020-09-22 12:14:11 -0700

    almost done

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const finalTip = document.querySelector('#final-tip');
+const finalTip = document.querySelector('#tipSpan');
 const totalCost = document.querySelector('#totalCostSpan');
 const billInput = document.querySelector('#bill');
 const tipInput = document.querySelector('#tip');
 const numPeople = document.querySelector('#num-people');
+const submit = document.querySelector('button');
 
 function calculateTip() {
     const billValue = billInput.value;
     const tipValue = tipInput.value;
     const people = numPeople.value;
     const tipAmount = (billValue * tipValue) / 100;
-    const tipPer = tipAmount / people;
-
-    console.log(tipPer);
-
-    // totalCost.innerText = tipAmount.toFixed(2);
+    const tipPer = parseInt(tipAmount) / parseInt(people);
+    const total = parseInt(billValue) + parseInt(tipAmount);
+    const totalPer = parseInt(total) / parseInt(people);
+    console.log(billValue);
+    console.log(tipAmount);
+    console.log(people);
+    console.log(total);
+    finalTip.innerText = tipPer.toFixed(2);
+    totalCost.innerText = totalPer.toFixed(2);
 }
 
-calculateTip();
-
-billInput.addEventListener('input', calculateTip);
-tipInput.addEventListener('input', calculateTip);
-numPeople.addEventListener('input', calculateTip);
+submit.addEventListener('click', function () {
+    calculateTip();
+    unHide();
+});
